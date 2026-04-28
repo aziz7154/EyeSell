0c84bcfc97f3799428a1ce5d3989353ecb904b9f
Aziz <saleh.58@wright.edu>
2026-04-27 20:07:48 -0400

    made minor adjustments to app.py

--- Backend/app.py
+++ Backend/app.py
@@ -19,8 +19,15 @@ UPLOAD_FOLDER = Path(__file__).parent / "uploads"
 UPLOAD_FOLDER.mkdir(exist_ok=True)
 ALLOWED_EXTENSIONS = {"jpg", "jpeg", "png", "webp"}
 
-CORS(app, supports_credentials=True, origins=["http://localhost:3000", "http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://localhost:5500", "http://localhost:5501"])
-
+CORS(app, supports_credentials=True, origins=[
+    "http://localhost:3000",
+    "http://localhost:5500",
+    "http://localhost:5501",
+    "http://localhost:8000",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5501",
+    "http://127.0.0.1:8000",
+])
 oauth = OAuth(app)
 
 google = oauth.register(
